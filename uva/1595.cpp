8d4abf855fdc9cd213559c0abdf090b07ebedbb1
zhangxj19 <120361640@qq.com>
2020-12-24 12:13:08 +0800

    pc

---
+++ uva/1595.cpp
@@ -0,0 +1,182 @@
+// #include <bits/stdc++.h>
+// can mix cin/cout with scanf/printf with debug mode
+// can only use cin/cout or scanf/printf without debug mode
+// notice:
+// 1) static map or tree can use Node
+// 2) dynamic map or tree can only use Node* 
+// 3) int bk[maxn] is much faster than unordered_set; bk << unordered_set << set
+// 4) int bk[maxn] = {0} is much faster than memset(bk, 0, sizeof(bk));
+// override the () operator
+// struct cmp{
+//     bool operator()(const T &a, const T &b) const{
+//         return ;
+//     }
+// };
+// some useful functions:
+// unique(it1, it2) upper_bound(it1, it2) lower_bound(it1, it2) equal_range(it1, it2)
+// next_permutation(it1, it2)
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <list>
+#include <map>
+#include <queue>
+#include <stack>
+#include <set>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <climits>
+
+#define rep(i, n) for (int i = 0, size = (n); i < size; ++i)
+#define repu(i, a, b) for (int i = (a), _upper_bound = (b); i < _upper_bound; ++i)
+#define repd(i, a, b) for (int i = (a), _lower_bound = (b); i > _lower_bound; --i)
+#define each(it, a) for(auto & (it) : (a))
+#define pf printf
+#define sf scanf
+#define _max(a, b) ((a) > (b) ? (a) : (b))
+#define _min(a, b) ((a) < (b) ? (a) : (b))
+
+typedef long long ll;
+const double eps = 1e-8;
+#define lowbit(x) (x&(-x))
+#define equ(a, b) (fabs(a - b) < eps)
+#define lcm(a, b) (a / gcd(a, b) * b)
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int, int>
+
+using namespace std;
+
+int gcd(int a, int b){
+    return !b ? a : gcd(b, a % b);
+}
+
+template<typename T>
+void print(vector<T> &v){
+    rep(i, v.size()){
+        if(i == 0) cout << v[i];
+        else cout << " " << v[i];
+    }
+    cout << endl;
+}
+
+template<typename T>
+void print(T* begin, T* end){
+    for(T *p = begin; p != end; ++p){
+        if(p == begin) cout << *p;
+        else cout << " " << *p;
+    }
+    cout << endl;
+}
+
+template<typename T>
+T sum(T* begin, T* end){
+    T re = 0;
+    for(T *p = begin; p != end; ++p) re = re + *p;
+    return re;
+}
+
+// template<typename T>
+// T sum(typename vector<T>::iterator begin, typename vector<T>::iterator end){
+//     T re = 0;
+//     for(auto p = begin; p != end; ++p) re = re + *p;
+//     return re;
+// }
+int sum(vector<int>::iterator begin, vector<int>::iterator end){
+    int re = 0;
+    for(auto p = begin; p != end; ++p) re = re + *p;
+    return re;
+}
+
+ll sum(vector<ll>::iterator begin, vector<ll>::iterator end){
+    ll re = 0;
+    for(auto p = begin; p != end; ++p) re = re + *p;
+    return re;
+}
+
+
+void solve(){
+    int T;
+    cin >> T;
+    while(T--){
+        
+        int n;
+        cin >> n;
+        vector<pii> a(n);
+        vi ax;
+        set<pii> S;
+        rep(i, n){
+            cin >> a[i].first >> a[i].second;
+            S.insert(a[i]);
+            ax.push_back(a[i].first);
+        }
+        sort(ax.begin(), ax.end());
+        int axsize = unique(ax.begin(), ax.end()) - ax.begin();
+        sort(a.begin(), a.end(), [](pii& p1, pii& p2){
+            return p1.first != p2.first ? p1.first < p2.first : p1.second < p2.second;
+        });
+        
+        if(axsize % 2 == 0){
+            double sx;
+            sx = (ax[(axsize-1) / 2] + ax[(axsize-1) / 2 + 1]) / 2.0;
+            int idx = lower_bound(a.begin(), a.end(), sx, [](const pii &p, double v){
+                return p.first < v;
+            }) - a.begin();
+            int find = 1;
+            rep(i, idx+1){
+                int nx = 2*sx - a[i].first, ny = a[i].second;
+                if(!S.count(make_pair(nx, ny))){
+                    find = 0;
+                    break;
+                }
+            }
+            if(find) cout << "YES" << endl;
+            else cout << "NO" << endl;           
+        }
+        else{
+            int sx = ax[(axsize-1) / 2];
+            int idx = lower_bound(a.begin(), a.end(), sx, [](const pii &p, int v){
+                return p.first < v;
+            }) - a.begin();
+            int find = 1;
+            rep(i, idx){
+                int nx = 2*sx - a[i].first, ny = a[i].second;
+                if(!S.count(make_pair(nx, ny))){
+                    find = 0;
+                    break;
+                }
+            }
+            if(find) cout << "YES" << endl;
+            else cout << "NO" << endl;
+
+        }
+        
+        
+    }
+}
+
+int main(){
+    #ifndef DEBUG
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    #endif
+    #ifdef LOCAL
+    freopen("in", "r", stdin);
+    freopen("o", "w", stdout);
+    #endif
+    // cout << setiosflags(ios::fixed);
+    // cout << setprecision(2);
+    // cout << setw(2) << setfill('0');  // add this every time when cout int with width and left padding '0'
+    solve();
+    
+    return 0;
+}
