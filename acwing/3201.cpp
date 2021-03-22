2e9a09b9e24749b5a73751acb6971131d89538f5
zhangxj19 <120361640@qq.com>
2021-03-22 12:10:50 +0800

    mac

--- acwing/3201.cpp
+++ acwing/3201.cpp
@@ -1,6 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
+const int N = 100;
+struct Node {
+    int x;
+    Node * nxt;
+}a[N];
+
+int sz;
+Node * newnode(int x) {
+    sz++;
+    Node * re = new Node;
+    re->x = x;
+    re->nxt = nullptr;
+    return re;
+}
+int m, n;
 
 signed main() {
     #ifdef LOCAL
@@ -9,5 +24,66 @@ signed main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
+
+    int x;
+    Node *head = newnode(-1);
+    Node *now;
+    now = head;
+    while(cin >> x, x != 0) {
+        Node *nn = newnode(x);
+        now->nxt = nn;
+        now = nn;
+    }
+    cin >> m >> n;
+
+    #ifdef DEBUG
+    now = head->nxt;
+    while(1) {
+        cout << now->x << " ";
+        if(now->nxt != nullptr) now = now->nxt;
+        else break;
+    }
+    cout << "\n";
+    #endif
+
+    now = head->nxt;
+    Node *prenow = head;
+    int cnt = 0;
+    
+    cnt = 0;
+    while(cnt != m) {
+        now = now->nxt;
+        prenow = prenow->nxt;
+        cnt++;
+    }
+    Node* pre = prenow, *pnxt = now;
+    Node* rear;
+
+    for(; cnt <= n; ++cnt) {
+        Node *t = now->nxt, *pt = now;
+        now->nxt = prenow;
+        prenow = pt;
+        if(cnt == n) pre->nxt = now;
+        now = t;
+        if(cnt == n) {
+            pnxt->nxt = now;
+        }
+    }
+
+    now = head;
+    while(now->nxt != nullptr) {
+        now = now->nxt;
+        cout << now->x << " ";
+        if(now == nullptr) break;
+    }
+    cout << "\n";
+
+    
+
+
+
+
+
+
     
 }
--- acwing/t.cpp
+++ acwing/t.cpp
@@ -1,59 +1,13 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
 #define int long long
-#define rep(i, n) for (int i = 0, size = (n); i < size; ++i)
-#define repu(i, a, b) for (int i = (a), _upper_bound = (b); i < _upper_bound; ++i)
-#define repd(i, a, b) for (int i = (a), _lower_bound = (b); i > _lower_bound; --i)
-#define each(it, a) for(auto & (it) : (a))
-#define pf printf
-#define sf scanf
-#define _max(a, b) ((a) > (b) ? (a) : (b))
-#define _min(a, b) ((a) < (b) ? (a) : (b))
 
-typedef long long ll;
-const double eps = 1e-8;
-#define lowbit(x) (x&(-x))
-#define equ(a, b) (fabs(a - b) < eps)
-#define lcm(a, b) (a / gcd(a, b) * b)
-#define vi vector<int>
-#define vvi vector<vector<int>>
-#define pii pair<int, int>
-
-int gcd(int a, int b){return !b ? a : gcd(b, a % b);}
-
-template<typename T>
-void print(vector<T> &v){rep(i, v.size()){if(i != 0) cout << " "; cout << v[i];}cout << '\n';}
-
-template<typename T>
-void print(T* begin, T* end){for(T *p = begin; p != end; ++p){if(p != begin) cout << " "; cout << *p;}cout << '\n';}
-
-template<typename T>
-T sum(T* begin, T* end){T re = 0;for(T *p = begin; p != end; ++p) re = re + *p;return re;}
-
-int sum(vector<int>::iterator begin, vector<int>::iterator end){int re = 0;for(auto p = begin; p != end; ++p) re = re + *p;return re;}
-
-int read(){int x; cin >> x; return x;}
-
-void solve(){
-    
-}
-
-signed main(){
-    #ifndef DEBUG
-    ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    #endif
+signed main() {
     #ifdef LOCAL
     freopen("in", "r", stdin);
-    freopen("o", "w", stdout);
     #endif
-    // cout << setiosflags(ios::fixed);
-    // cout << setprecision(2);
-    // cout << setw(2) << setfill('0');  // add this every time when cout int with width and left padding '0'
-    solve();
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     
-    return 0;
 }
