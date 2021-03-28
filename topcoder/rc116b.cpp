aad46386601b3a638f5bbbbc2d52194f3ea8a26f
zhangxj19 <120361640@qq.com>
2021-03-29 00:18:03 +0800

    mac

---
+++ topcoder/rc116b.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int mod = 998244353;
+
+signed main() {
+    #ifdef LOCAL
+    freopen("in", "r", stdin);
+    #endif
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+     
+}
