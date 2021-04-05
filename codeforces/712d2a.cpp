7283097d8e0eb87195a426ede1fe5be73648070c
zhangxj19 <120361640@qq.com>
2021-04-06 01:10:40 +0800

    mac

---
+++ codeforces/712d2a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    #ifdef LOCAL
+    freopen("in", "r", stdin);
+    #endif
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--) {
+        string s;
+        cin >> s;
+        
+    }
+    
+}
