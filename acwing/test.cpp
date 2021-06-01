359dce0696ab8e50a0728db95ed9fb37c8cd08be
zhangxj19 <120361640@qq.com>
2021-06-01 18:28:13 +0800

    fit-PC

--- acwing/test.cpp
+++ acwing/test.cpp
@@ -3,31 +3,6 @@ using namespace std;
 #define int long long
 #define pii pair<int, int>
 
-int solve(int u) {
-
-    int re = 0;
-    queue<pii> que({0, u}); // dist, u
-    unordered_set<int> vis;    
-    vis.insert(u);
-
-    while(!que.empty()) {
-        int x = que.front().second, d = que.front().first;  
-        que.pop();
-
-        re = max(re, d);
-
-        for(auto& v : g[x]) {
-            if(!vis.count(v)) {
-                que.push({d + 1, v});
-                vis.insert(v);
-            }
-                
-        }
-
-    }
-    return re;
-}
-
 signed main() {
     #ifdef LOCAL
     freopen("in", "r", stdin);
@@ -35,5 +10,13 @@ signed main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    
+    vector<string> msg {
+        "Hello",
+        "there",
+    };
+    for (const auto& word : msg) {
+        cout << word << " ";
+    }
+    cout << "\n";
+
 }
